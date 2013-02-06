dbc819b13494f0da757e409685f75126cf77ce59
snusnu <gamsnjaga@gmail.com>
2013-02-06 03:08:17 +0100

    Add specs for Registry#register

---
+++ spec/unit/substation/registry/register_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Registry, '#register' do
+  subject { object.register(action) }
+
+  let(:action) { mock }
+
+  context 'when the action is not yet registered' do
+    let(:object) { described_class.new }
+
+    it_behaves_like 'a command method'
+
+    its(:size) { should == 1 }
+
+    it { should include(action) }
+  end
+
+  context 'when the action is already registered' do
+    let(:object) { described_class.new(Set[action]) }
+
+    it_behaves_like 'a command method'
+
+    its(:size) { should == 1 }
+
+    it { should include(action) }
+  end
+end
