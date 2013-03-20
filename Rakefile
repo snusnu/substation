8c3d1b01d1339d9e8b7a6b18ca4427ce958c4ad7
snusnu <gamsnjaga@gmail.com>
2013-03-20 15:21:04 +0100

    Update to latest devtools

--- Rakefile
+++ Rakefile
@@ -1,12 +1,6 @@
 # encoding: utf-8
 
-require 'rubygems'
 require 'rake'
 require 'devtools'
 
-Devtools.init
-
-FileList['tasks/**/*.rake'].each { |task| import task }
-
-desc 'Default: run all specs'
-task :default => :spec
+Devtools.init_rake_tasks
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,31 +1,10 @@
-require 'backports'
-require 'backports/basic_object' unless defined?(BasicObject)
-require 'rubygems'
-
-if ENV['COVERAGE']
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter "/spec/"
-  end
-end
-
-require 'pp'
-require 'rspec'
-
+require 'devtools'
 require 'substation'
 
-ENV['TZ'] = 'UTC'
-
-%w[shared support].each do |name|
-  Dir[File.expand_path("../#{name}/**/*.rb", __FILE__)].each do |file|
-    require file
-  end
-end
-
-RSpec.configure do |config|
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true
-  config.include(SpecHelper)
-end
+Devtools.init_spec_helper
 
 include Substation
+
+RSpec.configure do |config|
+  config.include(SpecHelper)
+end
