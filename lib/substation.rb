9f63a26f353c77cb60180b1c714002ab5336749e
snusnu <gamsnjaga@gmail.com>
2013-05-07 20:06:57 +0200

    Remove constants that aren't in use anymore

--- lib/substation.rb
+++ lib/substation.rb
@@ -7,13 +7,6 @@ require 'abstract_type'
 require 'concord'
 
 module Substation
-
-  # An empty frozen hash
-  EMPTY_HASH = {}.freeze
-
-  # An empty frozen array
-  EMPTY_ARRAY = [].freeze
-
 end
 
 require 'substation/request'
