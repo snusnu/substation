78f7cc7d6940d11a06d51692e36900407f224c3a
snusnu <gamsnjaga@gmail.com>
2013-06-18 01:26:30 +0200

    Move Environment::DSL class to its own file

--- lib/substation/environment.rb
+++ lib/substation/environment.rb
@@ -3,59 +3,6 @@ module Substation
   # The environment holding all registered {Chain} processors
   class Environment
 
-    # The DSL class used to define register processors
-    class DSL
-
-      # The registry of processors
-      #
-      # @return [Hash<Symbol, #call>]
-      #
-      # @api private
-      attr_reader :registry
-
-
-      # The registry of processors
-      #
-      # @param [Proc] block
-      #   a block to be instance_eval'ed
-      #
-      # @return [Hash<Symbol, #call>]
-      #
-      # @api private
-      def self.registry(&block)
-        new(&block).registry
-      end
-
-      # Initialize a new instance
-      #
-      # @param [Proc] block
-      #   a block to be instance_eval'ed
-      #
-      # @return [undefined]
-      #
-      # @api private
-      def initialize(&block)
-        @registry = {}
-        instance_eval(&block) if block
-      end
-
-      # Register a new +processor+ using the given +name+
-      #
-      # @param [#to_sym] name
-      #   the name to register the +processor+ for
-      #
-      # @param [#call] processor
-      #   the processor to register for +name+
-      #
-      # @return [self]
-      #
-      # @api private
-      def register(name, processor)
-        @registry[name] = processor
-        self
-      end
-    end
-
     include Equalizer.new(:registry)
     include Adamantium::Flat
 
@@ -107,5 +54,6 @@ module Substation
     #
     # @api private
     attr_reader :registry
-  end
-end
+
+  end # class Environment
+end # module Substation
---
+++ lib/substation/environment/dsl.rb
@@ -0,0 +1,58 @@
+module Substation
+  class Environment
+
+    # The DSL class used to define register processors
+    class DSL
+
+      # The registry of processors
+      #
+      # @return [Hash<Symbol, #call>]
+      #
+      # @api private
+      attr_reader :registry
+
+
+      # The registry of processors
+      #
+      # @param [Proc] block
+      #   a block to be instance_eval'ed
+      #
+      # @return [Hash<Symbol, #call>]
+      #
+      # @api private
+      def self.registry(&block)
+        new(&block).registry
+      end
+
+      # Initialize a new instance
+      #
+      # @param [Proc] block
+      #   a block to be instance_eval'ed
+      #
+      # @return [undefined]
+      #
+      # @api private
+      def initialize(&block)
+        @registry = {}
+        instance_eval(&block) if block
+      end
+
+      # Register a new +processor+ using the given +name+
+      #
+      # @param [#to_sym] name
+      #   the name to register the +processor+ for
+      #
+      # @param [#call] processor
+      #   the processor to register for +name+
+      #
+      # @return [self]
+      #
+      # @api private
+      def register(name, processor)
+        @registry[name] = processor
+        self
+      end
+
+    end # class DSL
+  end # class Environment
+end # module Substation
