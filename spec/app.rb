snusnu/substation

commit d2bd081929331d1d4a01665118d4a1bf88fd1c0a

Refactor Chain::DSL#failure_chain to be simpler
The code now relies on ruby-19 ordered hashes.

The increased flay threshold is due to similar
code found in :defn

  substation/chain/dsl.rb:129
  substation/environment/dsl.rb:52

which i guess is just fair enough.

config/flay.yml
@@ -1,3 +1,3 @@
 ---
-threshold: 6
-total_score: 107
+threshold: 8
+total_score: 114

lib/substation/chain/dsl.rb
@@ -79,13 +79,6 @@ module Substation
 
       # The processors to be used within a {Chain}
       #
-      # @return [Array<#call>]
-      #
-      # @api private
-      attr_reader :processors
-
-      # The processors to be used within a {Chain}
-      #
       # @param [#each<#call>] processors
       #   the processors to build on top of
       #
@@ -111,9 +104,18 @@ module Substation
       #
       # @api private
       def initialize(processors, &block)
-        @processors = []
+        @processors = {}
         chain(processors)
         instance_eval(&block) if block
+      end
+
+      # The processors to be used within a {Chain}
+      #
+      # @return [Array<#call>]
+      #
+      # @api private
+      def processors
+        @processors.values
       end
 
       # Use the given +processor+ within a chain
@@ -125,7 +127,7 @@ module Substation
       #
       # @api private
       def use(processor)
-        @processors << processor
+        @processors[processor.name] = processor
         self
       end
 
@@ -154,7 +156,7 @@ module Substation
       #
       # @api private
       def failure_chain(name, chain)
-        replace_processor(processor(name), chain)
+        @processors[name] = processor(name).with_failure_chain(chain)
         self
       end
 
@@ -168,31 +170,16 @@ module Substation
       # @return [#call]
       #   the processor identified by +name+
       #
-      # @return [nil]
+      # @raise [UnknownProcessor]
       #   if no processor identified by +name+ is registered
       #
       # @api private
       def processor(name)
-        found = @processors.detect { |processor| processor.name == name }
-        unless found
+        @processors.fetch(name) {
           raise UnknownProcessor, "No processor named #{name.inspect} is registered"
-        end
-        found
+        }
       end
 
-      # Replace +processor+'s failure chain with +chain+
-      #
-      # @param [#call] processor
-      # @param [#call] chain
-      #
-      # @return [undefined]
-      #
-      # @api private
-      def replace_processor(processor, chain)
-        index = @processors.index(processor)
-        @processors.delete_at(index)
-        @processors.insert(index, processor.with_failure_chain(chain))
-      end
     end # class DSL
   end # class Chain
 end # module Substation

commit 834aa931e1d48b865029993a135a3adc9a4bfee6

Add a sample test app plus proper integration specs
TODO: fix pending integration specs by supporting
multiple chain processors with the same name. The
change in d2bd081929331d1d4a01665118d4a1bf88fd1c0a
broke that behavior, but the previous code wasn't
ideal either.

Gemfile
@@ -2,6 +2,12 @@ source 'http://rubygems.org'
 
 gemspec
 
+group :test do
+  gem 'ducktrap'
+  gem 'vanguard'
+  gem 'anima'
+end
+
 group :development do
   gem 'devtools', :git => 'https://github.com/rom-rb/devtools.git'
   eval File.read('Gemfile.devtools')

config/flay.yml
@@ -1,3 +1,3 @@
 ---
 threshold: 9
-total_score: 111
+total_score: 107

lib/substation/chain.rb
@@ -197,7 +197,7 @@ module Substation
           return response unless processor.success?(response)
           processor.result(response)
         rescue => exception
-          return on_failure(request, result, exception)
+          return on_failure(request, result.data, exception)
         end
       }
     end

spec/app.rb
@@ -1,0 +1,356 @@
+# encoding: utf-8
+
+require 'pp'
+
+require 'substation'
+require 'anima'
+require 'ducktrap'
+require 'vanguard'
+
+class Demo
+
+  Undefined = Object.new.freeze
+
+  module Handler
+
+    class Result
+      include Concord::Public.new(:output)
+
+      class Success < self
+        def success?
+          true
+        end
+      end
+
+      class Failure < self
+        def success?
+          false
+        end
+      end
+    end
+
+    class Authenticator
+
+      include Concord.new(:request)
+
+      def self.call(request)
+        new(request).call
+      end
+
+      def call
+        if request.input.name != 'unknown'
+          Result::Success.new(request.input)
+        else
+          Result::Failure.new(request.input)
+        end
+      end
+    end
+
+    class Authorizer
+
+      include Concord.new(:request)
+
+      def self.call(request)
+        new(request).call
+      end
+
+      def call
+        if request.input.name != 'forbidden'
+          Result::Success.new(request.input)
+        else
+          Result::Failure.new(request.input)
+        end
+      end
+    end
+  end
+
+  module Models
+    class Person
+      include Anima.new(:id, :name)
+    end
+  end
+
+  module Sanitizers
+    ID_TRAP = Ducktrap.build do
+      custom do
+        forward { |input| input.merge(:id => nil) }
+        inverse { |input|
+          input = input.dup
+          input.delete(:id)
+          input
+        }
+      end
+    end
+
+    NEW_PERSON = Ducktrap.build do
+      primitive(Hash)
+      hash_transform do
+        fetch_key('name') do
+          primitive(String)
+          dump_key(:name)
+        end
+      end
+      add(ID_TRAP)
+      anima_load(Models::Person)
+    end
+  end
+
+  module Validators
+    NEW_PERSON = Vanguard::Validator.build do
+      validates_presence_of :name
+      validates_length_of :name, :length => 3..20
+    end
+  end
+
+  module Actions
+    class CreatePerson
+      def self.call(request)
+        name = request.input.name
+        if name == 'error'
+          request.error(request.input)
+        elsif name == 'exception'
+          raise RuntimeError
+        else
+          request.success(request.input)
+        end
+      end
+    end
+  end
+
+  module Observers
+    LOG_EVENT  = ->(response) { }
+    SEND_EMAIL = ->(response) { }
+  end
+
+  class Presenter
+    include AbstractType
+    include Adamantium::Flat
+    include Concord.new(:data)
+
+    def method_missing(method, *args, &block)
+      @data.send(method, *args, &block)
+    end
+
+    def respond_to?(method)
+      super || @data.respond_to?(method)
+    end
+
+    class Collection < self
+
+      def self.member(presenter = Undefined)
+        return @member if presenter.equal?(Undefined)
+        @member = presenter
+      end
+
+      alias_method :entries, :data
+
+      protected :entries
+
+      def each
+        return to_enum unless block_given?
+        entries.each { |entry| yield self.class.member.new(entry) }
+        self
+      end
+    end
+  end
+
+  module Presenters
+
+    class Person < Presenter
+
+      def name
+        "Presenting: #{super}"
+      end
+
+      class Collection < Presenter::Collection
+        member(Person)
+        alias_method :people, :each
+      end
+    end
+  end
+
+  module Views
+
+    module Layout
+      def page_title
+        'substation demo'
+      end
+    end
+
+    class Person < Presenter
+      include Layout
+    end
+
+    class SanitizationError
+      include Concord::Public.new(:error)
+      include Layout
+    end
+
+    class InternalError
+      include Concord::Public.new(:data)
+    end
+  end
+
+  module Renderer
+
+    class SanitizationError
+      def self.call(response)
+        "Don't mess with the input params: #{response.input.inspect}"
+      end
+    end
+
+    class AuthenticationError
+      def self.call(response)
+        "Failed to authenticate: #{response.input.inspect}"
+      end
+    end
+
+    class AuthorizationError
+      def self.call(response)
+        "Failed to authorize: #{response.input.inspect}"
+      end
+    end
+
+    class ValidationError
+      def self.call(response)
+        "Failed to validate: #{response.input.inspect}"
+      end
+    end
+
+    class ApplicationError
+      def self.call(response)
+        "Failed to process: #{response.output.inspect}"
+      end
+    end
+
+    class InternalError
+      def self.call(response)
+        "Something bad happened: #{response.output.inspect}"
+      end
+    end
+  end
+
+  class Error
+    include Concord::Public.new(:object)
+    include Adamantium::Flat
+
+    InternalError       = Class.new(self)
+    ApplicationError    = Class.new(self)
+    SanitizationError   = Class.new(self)
+    AuthenticationError = Class.new(self)
+    AuthorizationError  = Class.new(self)
+    ValidationError     = Class.new(self)
+  end
+
+  ENV = Substation::Environment.build do
+    register :sanitize,     Substation::Processor::Evaluator::Data
+    register :authenticate, Substation::Processor::Evaluator::Request
+    register :authorize,    Substation::Processor::Evaluator::Request
+    register :validate,     Substation::Processor::Evaluator::Data
+    register :call,         Substation::Processor::Evaluator::Pivot
+    register :wrap,         Substation::Processor::Wrapper
+    register :render,       Substation::Processor::Transformer
+  end
+
+  CREATE_PERSON = ENV.action Actions::CreatePerson, [
+    Observers::LOG_EVENT,
+    Observers::SEND_EMAIL
+  ]
+
+  module App
+
+    AUTHENTICATION_ERROR = Demo::ENV.chain { wrap Error::AuthenticationError }
+    AUTHORIZATION_ERROR  = Demo::ENV.chain { wrap Error::AuthorizationError }
+    VALIDATION_ERROR     = Demo::ENV.chain { wrap Error::ValidationError }
+    APPLICATION_ERROR    = Demo::ENV.chain { wrap Error::ApplicationError }
+    INTERNAL_ERROR       = Demo::ENV.chain { wrap Error::InternalError }
+
+    AUTHENTICATE = ENV.chain do
+      authenticate Handler::Authenticator, AUTHENTICATION_ERROR
+    end
+
+    AUTHORIZE = ENV.chain(AUTHENTICATE) do
+      authorize Handler::Authorizer, AUTHORIZATION_ERROR
+    end
+
+    CREATE_PERSON = ENV.chain(AUTHORIZE) do
+      validate Validators::NEW_PERSON, VALIDATION_ERROR
+      call Demo::CREATE_PERSON, APPLICATION_ERROR
+    end
+
+  end
+
+  module Web
+
+    SANITIZATION_ERROR = Demo::ENV.chain { wrap Error::SanitizationError }
+
+    INTERNAL_ERROR = Demo::ENV.chain(App::INTERNAL_ERROR) do
+      wrap Views::InternalError
+    end
+
+    CREATE_PERSON = Demo::ENV.chain do
+      sanitize Sanitizers::NEW_PERSON, SANITIZATION_ERROR
+      chain App::CREATE_PERSON
+    end
+
+    module HTML
+
+      SANITIZATION_ERROR = Demo::ENV.chain(Web::SANITIZATION_ERROR) do
+        wrap   Views::SanitizationError
+        render Renderer::SanitizationError
+      end
+
+      AUTHENTICATION_ERROR = Demo::ENV.chain(App::AUTHENTICATION_ERROR) do
+        render Renderer::AuthenticationError
+      end
+
+      AUTHORIZATION_ERROR = Demo::ENV.chain(App::AUTHORIZATION_ERROR) do
+        render Renderer::AuthorizationError
+      end
+
+      VALIDATION_ERROR = Demo::ENV.chain(App::VALIDATION_ERROR) do
+        render Renderer::ValidationError
+      end
+
+      APPLICATION_ERROR = Demo::ENV.chain(App::APPLICATION_ERROR) do
+        render Renderer::ApplicationError
+      end
+
+      INTERNAL_ERROR  = Demo::ENV.chain(Web::INTERNAL_ERROR) do
+        render Renderer::InternalError
+      end
+
+      CREATE_PERSON = Demo::ENV.chain(Web::CREATE_PERSON, INTERNAL_ERROR) do
+        failure_chain :sanitize,     SANITIZATION_ERROR
+        failure_chain :authenticate, AUTHENTICATION_ERROR
+        failure_chain :authorize,    AUTHORIZATION_ERROR
+        failure_chain :validate,     VALIDATION_ERROR
+        failure_chain :call,         APPLICATION_ERROR
+        wrap Presenters::Person
+        wrap Views::Person
+      end
+
+    end # module HTML
+
+  end
+
+  ACTIONS = {
+
+    :HTML => {
+      :create_person   => Web::HTML::CREATE_PERSON,
+    }
+
+  }.freeze
+
+  APP_ENV = Object.new
+
+  include Concord.new(:dispatcher)
+  include Adamantium::Flat
+
+  APP = new(Substation::Dispatcher.new(ACTIONS[:HTML], APP_ENV))
+
+  def call(name, input = nil)
+    dispatcher.call(name, input)
+  end
+
+end # class Demo

spec/integration/app_spec.rb
@@ -1,0 +1,136 @@
+# encoding: utf-8
+
+require 'app'
+
+describe 'a typical substation application' do
+  subject { Demo::APP.call(name, input) }
+
+  let(:request) { Substation::Request.new(:create_person, env, input) }
+  let(:name)    { :create_person }
+  let(:env)     { Demo::APP_ENV }
+
+  shared_examples_for 'an action invocation' do
+    it 'notifies all observers' do
+      Demo::Observers::LOG_EVENT.should_receive(:call).with(action_response)
+      Demo::Observers::SEND_EMAIL.should_receive(:call).with(action_response)
+      subject
+    end
+  end
+
+  shared_examples_for 'no action invocation' do
+    it 'does not notify any observers' do
+      Demo::Observers::LOG_EVENT.should_not_receive(:call)
+      Demo::Observers::SEND_EMAIL.should_not_receive(:call)
+      subject
+    end
+  end
+
+  context 'with valid input' do
+    let(:input) { { 'name' => 'John' } }
+
+    let(:processed_request) { Substation::Request.new(name, env, processed_input) }
+    let(:processed_input)   { Demo::Models::Person.new(:id => nil, :name => 'John') }
+    let(:presenter)         { Demo::Presenters::Person.new(processed_input) }
+    let(:view)              { Demo::Views::Person.new(presenter) }
+    let(:response)          { Substation::Response::Success.new(processed_request, view) }
+
+    it_behaves_like 'an action invocation' do
+      let(:action_response) { Substation::Response::Success.new(processed_request, processed_input) }
+    end
+
+    pending { it { should eql(response) } }
+  end
+
+  context 'with an input that produces an application error' do
+    let(:input) { { 'name' => 'error' } }
+
+    let(:processed_request) { Substation::Request.new(name, env, processed_input) }
+    let(:processed_input)   { Demo::Models::Person.new(:id => nil, :name => 'error') }
+    let(:error)             { Demo::Error::ApplicationError.new(processed_input) }
+    let(:error_response)    { Substation::Response::Failure.new(processed_request, error) }
+    let(:rendered)          { Demo::Renderer::ApplicationError.call(error_response) }
+    let(:response)          { Substation::Response::Failure.new(processed_request, rendered) }
+
+    it_behaves_like 'an action invocation' do
+      let(:action_response) { Substation::Response::Failure.new(processed_request, processed_input) }
+    end
+
+    it { should eql(response) }
+  end
+
+  context 'with an input that raises an exception while processing' do
+    let(:input) { { 'name' => 'exception' } }
+
+    let(:processed_request) { Substation::Request.new(name, env, processed_input) }
+    let(:processed_input)   { Demo::Models::Person.new(:id => nil, :name => 'exception') }
+    let(:failure_data)      { Substation::Chain::FailureData.new(processed_input, RuntimeError.new) }
+    let(:error)             { Demo::Error::InternalError.new(failure_data) }
+    let(:view)              { Demo::Views::InternalError.new(error) }
+    let(:error_response)    { Substation::Response::Failure.new(processed_request, view) }
+    let(:rendered)          { Demo::Renderer::InternalError.call(error_response) }
+    let(:response)          { Substation::Response::Failure.new(request, rendered) }
+
+    it_behaves_like 'no action invocation'
+
+    pending { it { should eql(response) } }
+  end
+
+  context 'with invalid input' do
+    let(:input) { { 'name' => 'X' } }
+
+    let(:processed_request) { Substation::Request.new(name, env, processed_input) }
+    let(:processed_input)   { Demo::Models::Person.new(:id => nil, :name => 'X') }
+    let(:error)             { Demo::Error::ValidationError.new(processed_input) }
+    let(:error_response)    { Substation::Response::Failure.new(processed_request, error) }
+    let(:rendered)          { Demo::Renderer::ValidationError.call(error_response) }
+    let(:response)          { Substation::Response::Failure.new(processed_request, rendered) }
+
+    it_behaves_like 'no action invocation'
+
+    it { should eql(response) }
+  end
+
+  context 'with malformed input' do
+    let(:input) { { 'evil' => 'params' } }
+
+    let(:error)             { Demo::Error::SanitizationError.new(input) }
+    let(:view)              { Demo::Views::SanitizationError.new(error) }
+    let(:error_response)    { Substation::Response::Failure.new(request, view) }
+    let(:rendered)          { Demo::Renderer::SanitizationError.call(error_response) }
+    let(:response)          { Substation::Response::Failure.new(request, rendered) }
+
+    it_behaves_like 'no action invocation'
+
+    it { should eql(response) }
+  end
+
+  context 'with input from an unknown user' do
+    let(:input) { { 'name' => 'unknown' } }
+
+    let(:processed_request) { Substation::Request.new(name, env, processed_input) }
+    let(:processed_input)   { Demo::Models::Person.new(:id => nil, :name => 'unknown') }
+    let(:error)             { Demo::Error::AuthenticationError.new(processed_input) }
+    let(:error_response)    { Substation::Response::Failure.new(processed_request, error) }
+    let(:rendered)          { Demo::Renderer::AuthenticationError.call(error_response) }
+    let(:response)          { Substation::Response::Failure.new(processed_request, rendered) }
+
+    it_behaves_like 'no action invocation'
+
+    it { should eql(response) }
+  end
+
+  context 'with input from an unauthorized user' do
+    let(:input) { { 'name' => 'forbidden' } }
+
+    let(:processed_request) { Substation::Request.new(name, env, processed_input) }
+    let(:processed_input)   { Demo::Models::Person.new(:id => nil, :name => 'forbidden') }
+    let(:error)             { Demo::Error::AuthorizationError.new(processed_input) }
+    let(:error_response)    { Substation::Response::Failure.new(processed_request, error) }
+    let(:rendered)          { Demo::Renderer::AuthorizationError.call(error_response) }
+    let(:response)          { Substation::Response::Failure.new(processed_request, rendered) }
+
+    it_behaves_like 'no action invocation'
+
+    it { should eql(response) }
+  end
+end

spec/unit/substation/chain/call_spec.rb
@@ -103,7 +103,7 @@ describe Chain, '#call' do
       }
 
       let(:response) { Response::Failure.new(request, data) }
-      let(:data)     { Chain::FailureData.new(request, RuntimeError.new('exception_1')) }
+      let(:data)     { Chain::FailureData.new(input, RuntimeError.new('exception_1')) }
 
       it { should eql(response) }
 
@@ -166,7 +166,7 @@ describe Chain, '#call' do
       }
 
       let(:response)        { Response::Failure.new(request, data) }
-      let(:data)            { Chain::FailureData.new(current_request, RuntimeError.new('exception_2')) }
+      let(:data)            { Chain::FailureData.new(:success_1, RuntimeError.new('exception_2')) }
       let(:current_request) { Request.new(name, env, :success_1) }
 
       it { should eql(response) }
@@ -214,7 +214,7 @@ describe Chain, '#call' do
       }
 
       let(:response)         { Response::Failure.new(request, data) }
-      let(:data)             { Chain::FailureData.new(current_response, RuntimeError.new('exception_3')) }
+      let(:data)             { Chain::FailureData.new(:success_2, RuntimeError.new('exception_3')) }
       let(:current_request)  { Request.new(name, env, :success_1) }
       let(:current_response) { Response::Success.new(current_request, :success_2) }
 