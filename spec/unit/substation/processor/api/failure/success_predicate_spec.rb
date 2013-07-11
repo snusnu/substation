2beac60cab3d547bf27b472144e38eb4e39d89da
snusnu <gamsnjaga@gmail.com>
2013-07-11 14:27:50 +0200

    [ci skip] Fix whitespace in specs

--- spec/unit/substation/processor/api/failure/success_predicate_spec.rb
+++ spec/unit/substation/processor/api/failure/success_predicate_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe Processor::API::Failure, '#success?' do
   subject { object.success? }
 
-  let(:object)  { Class.new { include Processor::API::Failure }.new }
+  let(:object) { Class.new { include Processor::API::Failure }.new }
 
   it { should be(false) }
 end
--- spec/unit/substation/processor/api/success/success_predicate_spec.rb
+++ spec/unit/substation/processor/api/success/success_predicate_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe Processor::API::Success, '#success?' do
   subject { object.success? }
 
-  let(:object)  { Class.new { include Processor::API::Success }.new }
+  let(:object) { Class.new { include Processor::API::Success }.new }
 
   it { should be(true) }
 end
