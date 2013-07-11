4a5af7279a5e4d9e1dbe471709cf75ed51267230
snusnu <gamsnjaga@gmail.com>
2013-07-11 16:00:51 +0200

    Add an idiomatic environment to the integration spec
    This is actually just for "demonstration" purposes

--- spec/app.rb
+++ spec/app.rb
@@ -262,7 +262,44 @@ class Demo
 
   end
 
-  APP_ENV = Object.new
+  class Environment
+
+    def self.build(name, storage, logger)
+      coerced_name = name.to_sym
+      klass = case coerced_name
+      when :development
+        Development
+      when :test
+        Test
+      when :production
+        Production
+      end
+      klass.new(coerced_name, storage, logger)
+    end
+
+    class Development < self
+      def development?; true end
+    end
+
+    class Test < self
+      def test?; true end
+    end
+
+    class Production < self
+      def production?; true end
+    end
+
+    include Concord.new(:name, :storage, :logger)
+
+    def development?; false end
+    def test?;        false end
+    def production?;  false end
+  end
+
+  logger   = Object.new # some logger instance
+  storage  = Object.new # some database abstraction e.g. ROM::Environment
+  env_name = ::ENV.fetch('APP_ENV', :development)
+  APP_ENV  = Environment.build(env_name, storage, logger)
 
   module Web
 
