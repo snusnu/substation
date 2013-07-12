70cde3597df1d572c77059b842d6f635fb662dee
snusnu <gamsnjaga@gmail.com>
2013-07-12 03:38:39 +0200

    Adjust code layout in spec/app.rb

--- spec/app.rb
+++ spec/app.rb
@@ -11,6 +11,55 @@ class Demo
 
   Undefined = Object.new.freeze
 
+  class Environment
+
+    def self.build(name, storage, logger)
+      coerced_name = name.to_sym
+      klass = case coerced_name
+      when :development
+        Development
+      when :test
+        Test
+      when :production
+        Production
+      end
+      klass.new(coerced_name, storage, logger)
+    end
+
+    class Development < self
+      def development?
+        true
+      end
+    end
+
+    class Test < self
+      def test?
+        true
+      end
+    end
+
+    class Production < self
+      def production?
+        true
+      end
+    end
+
+    include AbstractType
+    include Concord.new(:name, :storage, :logger)
+
+    def development?
+      false
+    end
+
+    def test?
+      false
+    end
+
+    def production?
+      false
+    end
+  end
+
   module Handler
 
     def call(request)
@@ -296,55 +345,6 @@ class Demo
       call Demo::CREATE_PERSON, APPLICATION_ERROR
     end
 
-  end
-
-  class Environment
-
-    def self.build(name, storage, logger)
-      coerced_name = name.to_sym
-      klass = case coerced_name
-      when :development
-        Development
-      when :test
-        Test
-      when :production
-        Production
-      end
-      klass.new(coerced_name, storage, logger)
-    end
-
-    class Development < self
-      def development?
-        true
-      end
-    end
-
-    class Test < self
-      def test?
-        true
-      end
-    end
-
-    class Production < self
-      def production?
-        true
-      end
-    end
-
-    include AbstractType
-    include Concord.new(:name, :storage, :logger)
-
-    def development?
-      false
-    end
-
-    def test?
-      false
-    end
-
-    def production?
-      false
-    end
   end
 
   logger   = Object.new # some logger instance
