53f09c14a3ec582238936b59baa604c6cfec2d8a
snusnu <gamsnjaga@gmail.com>
2013-07-22 17:56:38 +0200

    Add specs for Processor::Builder#call

---
+++ spec/unit/substation/processor/builder/call_spec.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Processor::Builder, '#call' do
+  subject { object.call(handler, failure_chain) }
+
+  include_context 'Processor::Config#initialize'
+
+  let(:object)    { described_class.new(name, klass, executor) }
+  let(:name)      { double('name') }
+  let(:klass)     { double('klass') }
+  let(:processor) { double('processor') }
+
+  before do
+    expect(klass).to receive(:new).with(name, config).and_return(processor)
+  end
+
+  it { should eql(processor) }
+end
