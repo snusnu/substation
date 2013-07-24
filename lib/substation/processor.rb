f25f9486693168f1e1bed2b9a8f33667eb917bd5
snusnu <gamsnjaga@gmail.com>
2013-07-24 02:09:17 +0200

    Revert "Silence "private attribute?" warnings"
    This reverts commit e619f325298a1ab8f0a780a9beb3d47c6b153496.
    
    For whatever reason, changing the code as the commit did, made
    mutant fail to cover code it previously had covered just fine.
    
    See https://travis-ci.org/snusnu/substation/builds/9411656

--- lib/substation/processor.rb
+++ lib/substation/processor.rb
@@ -67,13 +67,14 @@ module Substation
       response
     end
 
+    private
+
     # Return handler
     #
     # @return [#call]
     #
     # @api private
     attr_reader :handler
-    private     :handler
 
     # Return failure chain
     #
@@ -81,7 +82,6 @@ module Substation
     #
     # @api private
     attr_reader :failure_chain
-    private     :failure_chain
 
     # Return executor
     #
@@ -89,7 +89,6 @@ module Substation
     #
     # @api private
     attr_reader :executor
-    private     :executor
 
     # Execute processor on state
     #
