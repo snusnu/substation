0b79c787d21dc7b0bad555295605796b3fc84313
snusnu <gamsnjaga@gmail.com>
2013-08-07 03:52:12 +0200

    Extract shared examples for integration specs

--- spec/integration/demo/web_spec.rb
+++ spec/integration/demo/web_spec.rb
@@ -32,28 +32,6 @@ describe 'a typical substation application' do
     end
   end
 
-  shared_examples_for 'an action invocation' do
-    it_behaves_like 'all invocations'
-
-    it 'notifies all observers' do
-      expect(Demo::Core::Observers::LOG_EVENT).to receive(:call).with(action_response).ordered
-      expect(Demo::Core::Observers::SEND_EMAIL).to receive(:call).with(action_response).ordered
-      subject
-    end
-  end
-
-  shared_examples_for 'no action invocation' do
-    let(:success_status) { false }
-
-    it_behaves_like 'all invocations'
-
-    it 'does not notify any observers' do
-      expect(Demo::Core::Observers::LOG_EVENT).to_not receive(:call)
-      expect(Demo::Core::Observers::SEND_EMAIL).to_not receive(:call)
-      subject
-    end
-  end
-
   context 'with registered chains' do
     let(:input)      { mock }
     let(:account_id) { authorized_id }
